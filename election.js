32a90ef0b91d178f7dc33866d72a77b1f6295294
marmo002 <marmo_02@hotmail.com>
2017-11-15 14:53:26 -0500

    Finish assignment

--- election.js
+++ election.js
@@ -1,5 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'JSON'
+  }).done(function(response){
+    // console.log(response.candidates);
+
+    var ul = document.getElementById('candidates');
+
+    response.candidates.forEach(function(candidate){
+      var li = document.createElement('li');
+      // li.innerHTML = "<span><strong>"+ candidate.name +" :</strong></span><span> "+ candidate.votes +"</span>";
+      // ul.append(li);
+      var spanName = document.createElement('span');
+      var strong = document.createElement('strong');
+      strong.innerText = candidate.name+": ";
+      spanName.append(strong);
+      li.append(spanName);
+      var spanVotes = document.createElement('span');
+      spanVotes.innerText = candidate.votes;
+      li.append(spanVotes);
+      ul.append(li);
+    });
+
+
+  });
+
 
 });
